e55b4e76b674be5c8f42feeec61b33880dc29bec
mithun <mithunhrm07@gamil.com>
2019-11-20 14:31:16 +0530

    push commit

---
+++ src/Components/Signin/Signin.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import './Signin.css';
+import images from "../img.jpeg";
+import { SocialIcon } from 'react-social-icons';
+class Signin extends Component {
+  render() {
+    return (
+      <div class="container">
+        <div class="sign1">
+          Signin form</div><br></br>
+        <img className="images" src={images}></img>
+        <form>
+          <input class="name1" type="text" name="email" placeholder="Name or Email"></input><br></br>
+          <input class="pass1" type="text" name="password" placeholder="Password"></input>
+        </form>
+        <div><br></br>
+
+          <input class="check1" type="checkbox"></input>
+          Remember me
+             </div>
+        <div class="pass2">
+          ForgetPassword?
+                 <div><br></br>
+            <button class="sign2">Signin</button>
+            <div class="account1"><br></br>
+              Create a New accout<br></br>
+              (or)
+              <div class="icons1">
+                <SocialIcon className="icons" url="http://twitter.com/jaketrent" />
+                <SocialIcon className="icons" url="http://facebook.com/jaketrent" />
+                <SocialIcon className="icons" url="http://google.com/jaketrent" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Signin;
---
+++ src/Components/Signup/Signup.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './Signup.css';
+
+class Signup extends Component {
+    render() {
+        return (
+            <div className="form-color">
+                <div>Signup form</div><br></br>
+                <div><input type="text" name="Name" placeholder="First name" /></div><br></br>
+                <div><input type="text" name="Name" placeholder="Last name" /></div><br></br>
+                <div><input type="text" name="gmail" placeholder="mithunhrm07@gamil.com" /></div><br></br>
+                <div><input type="text" name="Number" placeholder="enter your phone number" /></div><br></br>
+                <div><input type="text" name="Password" placeholder="enter your password" /></div><br></br>
+                <div><input type="text" name="conformation" placeholder="conform password" /></div><br></br>
+                <div><input type="checkbox" />I agree to terms and privacy policy</div><br></br>
+                <button id="background-color" type="submit" value="Submit" class="submit">Signup</button>
+                <div><a href="/signin">Signin</a></div>
+            </div>
+        );
+    }
+}
+
+export default Signup;
